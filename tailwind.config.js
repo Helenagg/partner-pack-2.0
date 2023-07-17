787f961b36b2cf6f82718c7743897dd5209bdb9e
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-07-17 14:55:08 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#3F7C3A',
+        secondary: '#49564E',
+        'primary-dark': '#3A6B34',
+        'secondary-light': '#315B2C',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: 'Roboto',
+      }
     },
   },
   plugins: [],
