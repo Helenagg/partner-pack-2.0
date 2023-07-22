876595daf1f2695051a98f21081978dc88fb7605
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-07-22 14:44:46 +0200

    flip card pending center back

--- components/Productos/Productos.jsx
+++ components/Productos/Productos.jsx
@@ -4,10 +4,21 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Productos = () => {
 
-    const tablets = {
-        front: ["F","r","a","n","c","o"," ","A",".","M","a","r","c","u","z","z","i"," "],
-        back: ["D","e","v","e","l","o","p","e","r"," ","B","a","c","k","e","n","d"," "],
-      }
+  const url = [
+    {
+      name: "cajas",
+      url: "/assets/img/contacto/enfardadora_automatica.webp",
+    },
+    {
+      name: "cajas",
+      url: "/assets/img/contacto/enfardadora_automatica.webp",
+    },
+    {
+      name: "cajas",
+      url: "/assets/img/contacto/enfardadora_automatica.webp",
+    },
+  ];
+
   return (
     <div className='container mx-auto px-5 py-8'>
       <div className='text-center mb-6'>
@@ -15,27 +26,40 @@ const Productos = () => {
         <div className='flex justify-center'>
           <hr className='border-secondary-light m-2 border-1 w-24 rounded-md' />
         </div>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-          <Image
-            src='/assets/img/contacto/enfardadora_automatica.webp'
-            alt=''
-            width={1042}
-            height={1038}
-            className='w-full'
-          />
-          <div className='px-6 py-4'>
-            <div className='font-bold text-xl mb-2'>Cajas</div>
+        <div className='flex-grow w-full flex items-center justify-center flex-col group cursor-pointer'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-1 my-4 w-full text-center'>
+            {url.map((element, index) => {
+              return (
+                <div
+                  className='relative max-w-sm mt-4 rounded-lg transform transition ease-in-out shadow-xl [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] duration-1000 delay-[var(--delay)]'
+                  style={{ "--delay": index * 0.1 + "s" }}
+                  key={index}
+                >
+                  <div>
+                    <Image
+                      src={element.url}
+                      alt={element.name}
+                      width={1042}
+                      height={1038}
+                      className='w-full rounded-lg'
+                    />
+                  </div>
+                  {/* <div className=''> */}
+                  <div className='absolute bg-white rounded-lg inset-0 h-full w-full [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+                    <div className='font-bold mb-2'>Cajas</div>
 
-            <a className='text-gray-700 text-base flex justify-center'>
-              Ver Producto
-              <AiOutlineArrowRight className='mx-4 mt-1' />
-            </a>
+                    <a className='text-gray-700 text-base flex justify-center'>
+                      Ver Producto
+                      <AiOutlineArrowRight className='mx-4 mt-1' />
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
-      
     </div>
-    
   );
 };
 
