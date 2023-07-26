014ef2d08a938e6a78c82c0f4346ffdcee9e921b
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-07-26 18:39:42 +0200

    home pending height

--- components/Home/Informacion.jsx
+++ components/Home/Informacion.jsx
@@ -12,40 +12,81 @@ const Informacion = () => {
         </div>
         <div className='container py-5'>
           <div className='flex flex-col md:flex-row mb-3 md:mb-0'>
-            <div className='my-0 md:my-5 lg:py-5 rounded-lg '>
-              <div id='1' className='rounded-lg relative'>
+            <div className='my-0 md:my-5 p-0 md:py-5 rounded-lg z-20'>
+              <div className='rounded-lg relative'>
                 <Image
                   src='/assets/img/informacion/papel-film.webp'
                   alt='Papel Film'
-                  width={1024}
-                  height={938}
-                  className='rounded-lg'
+                  width={1600}
+                  height={786}
+                  className='rounded-lg max-w-full max-h-full'
                 />
-                <div id='2' className='absolute rounded-lg top-0 left-0 w-full h-full bg-primary/50 flex flex-col items-center justify-center'>
-                <h1 className='text-xl font-bold  text-white mb-4'>
-                  ¿ Quieres más información ?
-                </h1>
-                <p className=' text-white'>
-                  Si quieres saber más sobre nosotros o nuestros productos...
-                  <br />
-                  Llámanos sin compromiso, te atenderemos encantados
-                </p>
-                <Link href={"/contacto"} className='btn-white mt-4'>
-                  Contacto
-                </Link>
+                <div className='absolute rounded-lg top-0 left-0 w-full h-full bg-primary/50 flex flex-col items-center justify-center'>
+                  <h1 className='text-xl font-bold text-white mb-4'>
+                    ¿ Quieres más información ?
+                  </h1>
+                  <p className=' text-white'>
+                    Si quieres saber más sobre nosotros o nuestros productos...
+                    <br />
+                    Llámanos sin compromiso, te atenderemos encantados
+                  </p>
+                  <Link href={"/contacto"} className='btn-white mt-4'>
+                    Contacto
+                  </Link>
+                </div>
               </div>
-              </div>
-  
             </div>
-            <div className='flex flex-row lg:flex-cols-6 min-h-[400px] -ml-8'>
+            <div
+              className='flex flex-row lg:flex-cols-6 min-h-[400px] mt-1 md:-ml-10'
+            >
               <div className='relative h-full'>
                 <Image
                   src='/assets/img/informacion/cajas.webp'
                   alt='Papel Film'
                   width={1024}
-                  height={938}
-                  className='rounded-lg object-cover'
+                  height={1365}
+                  className='rounded-lg object-cover max-w-full max-h-full'
                 />
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row mb-3 md:mb-0'>
+            <div
+              id='2'
+              className='flex flex-row lg:flex-cols-6 min-h-[400px] mt-1 md:-mr-10'
+            >
+              <div className='relative h-full'>
+                <Image
+                  src='/assets/img/informacion/robotica.webp'
+                  alt='Papel Film'
+                  width={1024}
+                  height={1365}
+                  className='rounded-lg object-cover max-w-full max-h-full'
+                />
+              </div>
+            </div>
+            <div id='1' className='my-0 md:my-5 p-0 md:py-5 rounded-lg z-20'>
+              <div className='rounded-lg relative'>
+                <Image
+                  src='/assets/img/informacion/empaquetadora.webp'
+                  alt='Papel Film'
+                  width={1600}
+                  height={786}
+                  className='rounded-lg max-w-full max-h-full'
+                />
+                <div className='absolute rounded-lg top-0 left-0 w-full h-full bg-primary/50 flex flex-col items-center justify-center'>
+                  <h1 className='text-xl font-bold  text-white mb-4'>
+                    ¿ Quieres más información ?
+                  </h1>
+                  <p className=' text-white'>
+                    Si quieres saber más sobre nosotros o nuestros productos...
+                    <br />
+                    Llámanos sin compromiso, te atenderemos encantados
+                  </p>
+                  <Link href={"/contacto"} className='btn-white mt-4'>
+                    Contacto
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
