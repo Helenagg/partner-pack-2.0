4b5decc9fd06ae9e1b762678453d4abd6ebb2c7a
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-07-28 17:38:06 +0200

    pending mobile version

--- app/conocenos/page.jsx
+++ app/conocenos/page.jsx
@@ -1,6 +1,8 @@
 import Cover from "@/components/Cover/Cover";
 import React from "react";
 import Conocenos from "@/components/Conocenos/Conocenos";
+import Formulario from "@/components/Home/Formulario";
+import HomeContacto from "@/components/Home/HomeContacto";
 
 const PageConocenos = () => {
   return (
@@ -9,6 +11,15 @@ const PageConocenos = () => {
         <Cover urlCover='/assets/img/cover/conocenos.webp' />
       </div>
       <Conocenos />
+      <div className='container mx-auto px-5 py-8 mt-60'>
+        <div className='text-center mb-2'>
+          <div className='flex justify-center'>
+            <hr className='border-secondary-light mb-4 border-1 w-24 rounded-md' />
+          </div>
+          <Formulario />
+        </div>
+      </div>
+      <HomeContacto />
     </div>
   );
 };
