8b786779d24a27a30e10ebe739d6e01d209fcc74
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2023-11-19 18:32:06 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,3 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
-module.exports = {
-    // Otras configuraciones de Next.js...
-    swc1ache: {
-      navigateFallbackWhitelist: [/^\/(actualidad|conocenos|contacto|productos|sostenibilidad)/],
-    },
-  }
